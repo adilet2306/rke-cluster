74682958fd25e211eaa629d70d6ee13ba6e093aa
adilet2306 <adilet.abdraimov9@gmail.com>
2024-12-05 23:26:29 -0600

    added rke creation

---
+++ rke/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    rke = {
+      source = "rancher/rke"
+      version = ">= 1.0.0"  # Adjust the version to your needs
+    }
+  }
+}
+provider "rke" {}
+
+resource "rke_cluster" "cluster" {
+  nodes {
+    address          = "18.234.133.249"
+    user             = "ubuntu"
+    role             = ["controlplane", "etcd"]
+    ssh_key_path     = "~/.ssh/id_rsa"
+  }
+
+    nodes {
+    address          = "52.90.221.222"
+    user             = "ubuntu"
+    role             = ["worker"]
+    ssh_key_path     = "~/.ssh/id_rsa"
+  }
+
+    nodes {
+    address          = "54.160.243.10"
+    user             = "ubuntu"
+    role             = ["worker"]
+    ssh_key_path     = "~/.ssh/id_rsa"
+  }
+
+
+enable_cri_dockerd = true
+
+}
+
+output kube_config {
+    sensitive = true
+    value = rke_cluster.cluster.kube_config_yaml
+}
