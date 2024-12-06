3b6e179c900a20b7108a1bf18e3e615b2110ca39
adilet2306 <adilet.abdraimov9@gmail.com>
2024-12-05 23:02:13 -0600

    modified script

--- create-instances/script.sh
+++ create-instances/script.sh
@@ -2,7 +2,7 @@
 
 
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -16,7 +16,7 @@ sudo apt-get update
 sleep 5
 
 VERSION_STRING=5:24.0.9-1~ubuntu.22.04~jammy
-sudo apt-get install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo usermod -aG docker ubuntu
 
