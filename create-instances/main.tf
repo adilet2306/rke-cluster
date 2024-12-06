4b23f2d1474edeb1cadfa58f939eddbf300fb4f0
adilet2306 <adilet.abdraimov9@gmail.com>
2024-12-05 22:31:40 -0600

    new code

--- create-instances/main.tf
+++ create-instances/main.tf
@@ -15,11 +15,16 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 0
   ip_protocol       = "tcp"
-  to_port           = 65536
+  to_port           = 65535
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "rke-key"
+  public_key = file("~/.ssh/id_rsa.pub")
 }
